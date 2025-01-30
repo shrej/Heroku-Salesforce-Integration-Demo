2b62d0c7840f3331e7b459e19cf06286738b851c
Shreyans Jain <shrej3@gmail.com>
2025-01-30 11:25:01 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Heroku Salesforce Demo Integration App
---
+++ src/routes/index.js
@@ -0,0 +1,47 @@
+"use strict";
+const axios = require("axios");
+const pdf = require("pdf-parse");
+
+module.exports = async function (fastify, opts) {
+  /**
+   *
+   * @param request
+   * @param reply
+   * @returns {Promise<void>}
+   */
+  fastify.get("/parsefile", async function (request, reply) {
+    const { event, context, logger } = request.sdk;
+    const { org } = context;
+
+    const versionDataUrl = `/services/data/v${org.apiVersion}/sobjects/ContentVersion/${request.query.contentVersionId}/VersionData`;
+    const finalUrl = org.domainUrl + versionDataUrl;
+    const result = await fetchPdfData(finalUrl, org.dataApi.accessToken);
+    return {
+      data: result,
+    };
+  });
+
+  const fetchPdfData = async (orgBaseUrl, accessToken) => {
+    try {
+      // Fetch the ArrayBuffer using axios
+      const response = await axios.get(orgBaseUrl, {
+        responseType: "arraybuffer",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const arrayBuffer = response.data;
+
+      // Process the ArrayBuffer using pdf.js
+      const data = await pdf(arrayBuffer);
+      return data.text;
+    } catch (error) {
+      console.error(error); // Handle any errors that occur
+    }
+  };
+
+  fastify.setErrorHandler(function (error, request, reply) {
+    request.log.error(error);
+    reply.status(500).send({ code: "500", message: error.message });
+  });
+};
